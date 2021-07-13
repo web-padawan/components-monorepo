25c9d31e30e5f0d6c8fa14b0cd577050b645ec10
web-padawan <iamkulykov@gmail.com>
2021-07-13 16:38:27 +0300

    chore: add missing mixins to entrypoint

--- packages/field-base/index.d.ts
+++ packages/field-base/index.d.ts
@@ -1,6 +1,12 @@
+export { ClearButtonMixin } from './src/clear-button-mixin.js';
 export { DelegateFocusMixin } from './src/delegate-focus-mixin.js';
 export { DisabledMixin } from './src/disabled-mixin.js';
+export { FieldAriaMixin } from './src/field-aria-mixin.js';
 export { FocusMixin } from './src/focus-mixin.js';
+export { HelperTextMixin } from './src/helper-text-mixin.js';
+export { InputAriaMixin } from './src/input-aria-mixin.js';
 export { InputMixin } from './src/input-mixin.js';
+export { InputPropsMixin } from './src/input-props-mixin.js';
 export { LabelMixin } from './src/label-mixin.js';
 export { SlotMixin } from './src/slot-mixin.js';
+export { ValidateMixin } from './src/validate-mixin.js';
